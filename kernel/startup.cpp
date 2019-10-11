d84fb62a27e4765421297e190274de8ec9a9ba79
Rink Springer <rink@rink.nu>
2019-10-11 18:40:55 +0200

    Fix #GP when IRQ occurs after a syscall
    The scenario is:
    
    1. Userland invoked SYSCALL
    2. Kernel does stuff, SYSRET
    3. An interrupt occured
    4. #GP
    
    The problem was that due to a silly parenthesis mismatch when setting MSR_STAR,
    %cs/%ss would not be properly set. This would go unnoticed (as the segment
    limits are overriden during SYSRET), but once the interrupt would reach 'iretq',
    the CPU would reject the descriptors and #GP would occur
    
    :-/

--- kernel/startup.cpp
+++ kernel/startup.cpp
@@ -325,10 +325,10 @@ namespace
 
     void InitializeSyscall()
     {
-        wrmsr(
-            msr::STAR, ((static_cast<uint64_t>(Selector::UserCode) - 0x10) |
-                        (static_cast<uint64_t>(DescriptorPrivilege::User)) << 48L) |
-                           (static_cast<uint64_t>(Selector::KernelCode) << 32L));
+        constexpr auto star =
+            (static_cast<uint64_t>((static_cast<int>(Selector::UserCode) - 0x10) | static_cast<int>(DescriptorPrivilege::User)) << 48L) |
+                           (static_cast<uint64_t>(Selector::KernelCode) << 32L);
+        wrmsr(msr::STAR, star);
         wrmsr(msr::LSTAR, reinterpret_cast<uint64_t>(&syscall_handler));
         wrmsr(msr::SFMASK, 0x200); // IF
         wrmsr(msr::EFER, rdmsr(msr::EFER) | msr::EFER_SCE);
