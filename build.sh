dd02d8cad07735c78965ee5d1f5140e9b92d0863
Rink Springer <rink@rink.nu>
2019-10-30 20:11:06 +0100

    Add cross-building of binutils/gcc

--- build.sh
+++ build.sh
@@ -8,50 +8,49 @@ MAKE_ARGS=-j8
 
 # parse options
 CLEAN=0
-BINUTILS=0
-GCC=0
-NEWLIB=0
+TOOLCHAIN_HOST=0
+LIBS=0
 DASH=0
 COREUTILS=0
 INIT=0
+TOOLCHAIN_TARGET=0
 while [ "$1" != "" ]; do
 	P="$1"
 	case "$P" in
 		-h)
-			echo "usage: build.sh [-hc] [-bg] [-ndui]"
+			echo "usage: build.sh [-hca] [-t] [-l] [-duiT]"
             echo ""
             echo " -h    this help"
             echo " -c    clean (forces a rebuild of toolchain)"
             echo " -a    build everything"
             echo ""
-            echo " -b    build: binutils"
-            echo " -g    build: gcc"
+            echo " -t    build: binutils/gcc/libstdc++ for host"
             echo ""
-            echo " -n    build: newlib"
+            echo " -l    build: libraries for host"
+            echo ""
             echo " -d    build: dash"
             echo " -u    build: coreutils"
             echo " -i    build: init"
+            echo ""
+            echo " -T    build: binutils+gcc for target"
 			exit 1
 			;;
         -a)
-            BINUTILS=1
-            GCC=1
-            NEWLIB=1
+            TOOLCHAIN_HOST=1
+            LIBS=1
             DASH=1
             COREUTILS=1
             INIT=1
+            TOOLCHAIN_TARGET=1
             ;;
         -c)
             CLEAN=1
 			;;
-        -b)
-            BINUTILS=1
-			;;
-        -g)
-            GCC=1
+        -t)
+            TOOLCHAIN_HOST=1
             ;;
-        -n)
-            NEWLIB=1
+        -l)
+            LIBS=1
             ;;
         -d)
             DASH=1
@@ -61,6 +60,9 @@ while [ "$1" != "" ]; do
             ;;
         -i)
             INIT=1
+            ;;
+        -T)
+            TOOLCHAIN_TARGET=1
             ;;
         *)
             echo "unexpected parameter '$P'; use '-h' for help"
@@ -79,23 +81,8 @@ mkdir -p ${OUTDIR}
 TOOLCHAIN=`realpath ${TOOLCHAIN}`
 OUTDIR=`realpath ${OUTDIR}`
 
-# dependencies
-if [ ! -f ${TOOLCHAIN}/bin/${TARGET}-ld ]; then
-    echo "Note: enabling binutils build since it doesn't seem available"
-    BINUTILS=1
-fi
-if [ ! -f ${TOOLCHAIN}/bin/${TARGET}-gcc ]; then
-    echo "Note: enabling gcc build since it doesn't seem available"
-    GCC=1
-fi
-if [ ! -f ${TOOLCHAIN}/usr/lib/libc.a ]; then
-    echo "Note: enabling newlib build since it doesn't seem available"
-    NEWLIB=1
-fi
-
-# binutils
-if [ "$BINUTILS" -ne 0 ]; then
-    echo "*** Building binutils"
+if [ "$TOOLCHAIN_HOST" -ne 0 ]; then
+    echo "*** Building binutils (host)"
     rm -rf build/binutils
     mkdir -p build/binutils
     cd build/binutils
@@ -108,16 +95,18 @@ fi
 export PATH=${TOOLCHAIN}/bin:${PATH}
 
 # gcc
-if [ "$GCC" -ne 0 ]; then
-    echo "*** Building gcc"
+if [ "$TOOLCHAIN_HOST" -ne 0 ]; then
+    echo "*** Building gcc (host)"
     rm -rf build/gcc
     mkdir -p build/gcc
     cd build/gcc
-    ../../userland/gcc-9.2.0/configure --target=${TARGET} --disable-nls --without-headers --enable-languages='c,c++' --prefix=${TOOLCHAIN}
-    make ${MAKE_ARGS} all-gcc
-    make ${MAKE_ARGS} all-target-libgcc
-    make ${MAKE_ARGS} install-gcc
-    make ${MAKE_ARGS} install-target-libgcc
+    ../../userland/gcc-9.2.0/configure --target=${TARGET} --disable-nls --without-headers --enable-languages='c,c++' --prefix=${TOOLCHAIN} --disable-libstdcxx --disable-build-with-cxx --disable-libssp --disable-libquadmath
+    #make ${MAKE_ARGS} all-gcc
+    #make ${MAKE_ARGS} all-target-libgcc
+    #make ${MAKE_ARGS} install-gcc
+    #make ${MAKE_ARGS} install-target-libgcc
+    make ${MAKE_ARGS}
+    make ${MAKE_ARGS} install
     cd ../..
 fi
 
@@ -134,8 +123,8 @@ set(CMAKE_ASM_COMPILER \${CMAKE_C_COMPILER})
 fi
 
 # Newlib
-if [ "$NEWLIB" -ne 0 ]; then
-    echo "*** Building newlib"
+if [ "$LIBS" -ne 0 ]; then
+    echo "*** Building newlib (toolchain)"
     rm -rf build/newlib
     mkdir build/newlib
     cd build/newlib
@@ -145,9 +134,23 @@ if [ "$NEWLIB" -ne 0 ]; then
     cd ../..
 fi
 
+# libstdc++
+if [ "$LIBS" -ne 0 ]; then
+    echo "*** Building libstdcxx (toolchain)"
+    rm -rf build/libstdcxx
+    mkdir -p build/libstdcxx
+    cd build/libstdcxx
+    CFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/gcc-9.2.0/libstdc++-v3/configure --host=${TARGET} --target=${TARGET} --prefix=${TOOLCHAIN}
+    make ${MAKE_ARGS}
+    make ${MAKE_ARGS} install
+    # move c++ includes to where g++ can actually find them
+    mv ${TOOLCHAIN}/include/c++ ${TOOLCHAIN}/usr/include
+    cd ../..
+fi
+
 # Dash
 if [ "$DASH" -ne 0 ]; then
-    echo "*** Bulding dash"
+    echo "*** Building dash (target)"
     cd userland/dash-0.5.10.2
     make clean || true
     CFLAGS="--sysroot ${TOOLCHAIN} -DJOBS=0" ./configure --host=${TARGET} --prefix=${OUTDIR}
@@ -159,7 +162,7 @@ fi
 
 # coreutils
 if [ "$COREUTILS" -ne 0 ]; then
-    echo "*** Building coreutils"
+    echo "*** Building coreutils (target)"
     cd userland/coreutils-8.31
     make clean || true
     CFLAGS="--sysroot ${TOOLCHAIN} -DOK_TO_USE_1S_CLOCK" ./configure --host=${TARGET} --prefix=${OUTDIR}
@@ -170,7 +173,7 @@ fi
 
 # init
 if [ "$INIT" -ne 0 ]; then
-    echo "*** Building init"
+    echo "*** Building init (target)"
     rm -rf build/init
     mkdir -p build/init
     cd build/init
@@ -179,4 +182,28 @@ if [ "$INIT" -ne 0 ]; then
     cd ../..
 fi
 
+if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+    echo "*** Building binutils (target)"
+    rm -rf build/binutils-target
+    mkdir -p build/binutils-target
+    cd build/binutils-target
+    CFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/binutils-2.32/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --prefix=${OUTDIR}
+    make ${MAKE_ARGS}
+    make ${MAKE_ARGS} install
+    cd ../..
+fi
 
+if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+    echo "*** Building gcc (target)"
+    rm -rf build/gcc-target
+    mkdir -p build/gcc-target
+    cd build/gcc-target
+    CFLAGS="--sysroot ${TOOLCHAIN}" CXXFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/gcc-9.2.0/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --enable-languages='c,c++' --prefix=${OUTDIR}
+    # XXX for some reason, we need to explicitely configure/build the host
+    # libcpp; otherwise it inherits our sysroot which messes things up...
+    make ${MAKE_ARGS} configure-build-libcpp
+    (cd build-x86_64-pc-linux-gnu/libcpp && make ${MAKE_ARGS})
+    make ${MAKE_ARGS}
+    make ${MAKE_ARGS} install
+    cd ../..
+fi
