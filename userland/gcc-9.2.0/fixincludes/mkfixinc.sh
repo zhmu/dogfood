5c44218d4e4753776fa7621fae5f90cb96ebb4f4
Rink Springer <rink@rink.nu>
2019-10-30 19:09:06 +0100

    No need to fixincludes, so skip it

--- userland/gcc-9.2.0/fixincludes/mkfixinc.sh
+++ userland/gcc-9.2.0/fixincludes/mkfixinc.sh
@@ -19,7 +19,7 @@ case $machine in
     powerpc-*-rtems*   | \
     powerpcle-*-eabisim* | \
     powerpcle-*-eabi* | \
-    *-musl* )
+    *-musl* | *-dogfood*)
 	#  IF there is no include fixing,
 	#  THEN create a no-op fixer and exit
 	(echo "#! /bin/sh" ; echo "exit 0" ) > ${target}
