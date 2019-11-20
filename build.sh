e68660a0a6ebf48b16519fab204335f805265b9f
Rink Springer <rink@rink.nu>
2019-11-20 07:25:48 +0100

    Clean up build script
    Main benefit is that this properly splits the toolchain and the sysroot instead
    of just throwing them together. This is a first step in containerization of the
    build infrastructure.

--- build.sh
+++ build.sh
@@ -81,12 +81,15 @@ mkdir -p ${OUTDIR}
 TOOLCHAIN=`realpath ${TOOLCHAIN}`
 OUTDIR=`realpath ${OUTDIR}`
 
-if [ "$TOOLCHAIN_HOST" -ne 0 ]; then
-    echo "*** Building binutils (host)"
+SYSROOT=/tmp/dogfood-sysroot
+mkdir -p ${SYSROOT}/usr/include
+
+if [ ! -f "${TOOLCHAIN}/bin/${TARGET}-ld" ]; then
+    echo "*** Building binutils (toolchain)"
     rm -rf build/binutils
     mkdir -p build/binutils
     cd build/binutils
-    ../../userland/binutils-2.32/configure --target=${TARGET} --disable-nls --disable-werror --prefix=${TOOLCHAIN}
+    ../../userland/binutils-2.32/configure --target=${TARGET} --disable-nls --disable-werror --prefix=${TOOLCHAIN} --with-sysroot=${SYSROOT}
     make ${MAKE_ARGS}
     make ${MAKE_ARGS} install
     cd ../..
@@ -95,24 +98,22 @@ fi
 export PATH=${TOOLCHAIN}/bin:${PATH}
 
 # gcc
-if [ "$TOOLCHAIN_HOST" -ne 0 ]; then
-    echo "*** Building gcc (host)"
+if [ ! -f "${TOOLCHAIN}/bin/${TARGET}-gcc" ]; then
+    echo "*** Building gcc (toolchain)"
     rm -rf build/gcc
     mkdir -p build/gcc
     cd build/gcc
-    ../../userland/gcc-9.2.0/configure --target=${TARGET} --disable-nls --without-headers --enable-languages='c,c++' --prefix=${TOOLCHAIN} --disable-libstdcxx --disable-build-with-cxx --disable-libssp --disable-libquadmath
-    #make ${MAKE_ARGS} all-gcc
-    #make ${MAKE_ARGS} all-target-libgcc
-    #make ${MAKE_ARGS} install-gcc
-    #make ${MAKE_ARGS} install-target-libgcc
-    make ${MAKE_ARGS}
-    make ${MAKE_ARGS} install
+    ../../userland/gcc-9.2.0/configure --target=${TARGET} --disable-nls --without-headers --enable-languages='c,c++' --prefix=${TOOLCHAIN} --disable-libstdcxx --disable-build-with-cxx --disable-libssp --disable-libquadmath --with-sysroot=${SYSROOT} --with-gxx-include-dir=${SYSROOT}/usr/include/c++/9.2.0
+    # we can't build everything yet as libgcc requires header files from libc;
+    # settle for just gcc now
+    make ${MAKE_ARGS} all-gcc
+    make ${MAKE_ARGS} install-gcc
     cd ../..
 fi
 
 if [ ! -f ${TOOLCHAIN_FILE} ]; then
     echo "set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSROOT ${TOOLCHAIN})
+set(CMAKE_SYSROOT ${SYSROOT})
 set(CMAKE_WARN_DEPRECATED OFF)
 include(CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER(x86_64-elf-dogfood-gcc GNU)
@@ -122,51 +123,57 @@ set(CMAKE_ASM_COMPILER \${CMAKE_C_COMPILER})
 " >> ${TOOLCHAIN_FILE}
 fi
 
-# Newlib
-if [ "$LIBS" -ne 0 ]; then
-    echo "*** Building newlib (toolchain)"
-    rm -rf build/newlib
-    mkdir build/newlib
-    cd build/newlib
-    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${TOOLCHAIN} ../../lib/newlib-3.1.0
-    ninja install
-    cd ../..
-fi
-
-if [ "$LIBS" -ne 0 ]; then
-    echo "*** Installing kernel headers (target)"
+if [ ! -f "${SYSROOT}/usr/include/dogfood/types.h" ]; then
+    echo "*** Installing kernel headers (sysroot)"
     rm -rf build/headers-target
     mkdir build/headers-target
     cd build/headers-target
-    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${TOOLCHAIN} ../../kernel-headers
+    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${SYSROOT} ../../kernel-headers
     ninja install
     cd ../..
 fi
 
-if [ ! -d ${OUTDIR}/${TARGET}/include ]; then
-    ln -sf ${TOOLCHAIN}/usr/include ${TOOLCHAIN}/${TARGET}/include
+
+if [ ! -f "${SYSROOT}/usr/lib/libc.a" ]; then
+    echo "*** Building newlib (sysroot)"
+    rm -rf build/newlib
+    mkdir build/newlib
+    cd build/newlib
+    cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${SYSROOT} ../../lib/newlib-3.1.0
+    ninja install
+    cd ../..
 fi
 
+if [ ! -f "${TOOLCHAIN}/lib/gcc/${TARGET}/9.2.0/libgcc.a" ]; then
+    echo "*** Building libgcc (sysroot)"
+    cd build/gcc
+    make ${MAKE_ARGS} all-target-libgcc
+    make ${MAKE_ARGS} install-target-libgcc
+    cd ../..
+fi
+
+#if [ ! -d ${OUTDIR}/${TARGET}/include ]; then
+#    ln -sf ${TOOLCHAIN}/usr/include ${TOOLCHAIN}/${TARGET}/include
+#fi
+
 # libstdc++
-if [ "$LIBS" -ne 0 ]; then
+if [ ! -f "${SYSROOT}/usr/lib/libstdc++.a" ]; then
     echo "*** Building libstdcxx (toolchain)"
     rm -rf build/libstdcxx
     mkdir -p build/libstdcxx
     cd build/libstdcxx
-    CFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/gcc-9.2.0/libstdc++-v3/configure --host=${TARGET} --target=${TARGET} --prefix=${TOOLCHAIN}
+    CFLAGS="--sysroot ${SYSROOT}" ../../userland/gcc-9.2.0/libstdc++-v3/configure --host=${TARGET} --target=${TARGET} --prefix=${SYSROOT}/usr
     make ${MAKE_ARGS}
     make ${MAKE_ARGS} install
-    # move c++ includes to where g++ can actually find them
-    mv ${TOOLCHAIN}/include/c++ ${TOOLCHAIN}/usr/include
     cd ../..
 fi
 
 # Dash
-if [ "$DASH" -ne 0 ]; then
+if [ ! -f "${OUTDIR}/bin/sh" ]; then
     echo "*** Building dash (target)"
     cd userland/dash-0.5.10.2
     make clean || true
-    CFLAGS="--sysroot ${TOOLCHAIN} -DJOBS=0" ./configure --host=${TARGET} --prefix=${OUTDIR}
+    CFLAGS="--sysroot ${SYSROOT} -DJOBS=0" ./configure --host=${TARGET} --prefix=${OUTDIR}
     make ${MAKE_ARGS}
     make install
     mv ${OUTDIR}/bin/dash ${OUTDIR}/bin/sh
@@ -174,18 +181,18 @@ if [ "$DASH" -ne 0 ]; then
 fi
 
 # coreutils
-if [ "$COREUTILS" -ne 0 ]; then
+if [ ! -f "${OUTDIR}/bin/ls" ]; then
     echo "*** Building coreutils (target)"
     cd userland/coreutils-8.31
     make clean || true
-    CFLAGS="--sysroot ${TOOLCHAIN} -DOK_TO_USE_1S_CLOCK" ./configure --host=${TARGET} --prefix=${OUTDIR}
+    CFLAGS="--sysroot ${SYSROOT} -DOK_TO_USE_1S_CLOCK" ./configure --host=${TARGET} --prefix=${OUTDIR}
     make ${MAKE_ARGS}
     make install
     cd ../..
 fi
 
 # init
-if [ "$INIT" -ne 0 ]; then
+if [ ! -f "${OUTDIR}/sbin/init" ]; then
     echo "*** Building init (target)"
     rm -rf build/init
     mkdir -p build/init
@@ -195,52 +202,51 @@ if [ "$INIT" -ne 0 ]; then
     cd ../..
 fi
 
-if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+if [ ! -f "${OUTDIR}/bin/ld" ]; then
     echo "*** Building binutils (target)"
     rm -rf build/binutils-target
     mkdir -p build/binutils-target
     cd build/binutils-target
-    CFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/binutils-2.32/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --prefix=${OUTDIR}
+    CFLAGS="--sysroot ${SYSROOT}" ../../userland/binutils-2.32/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --prefix=/usr
     make ${MAKE_ARGS}
-    make ${MAKE_ARGS} install
+    make ${MAKE_ARGS} install DESTDIR=${OUTDIR}
     cd ../..
 fi
 
-if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+if [ ! -f "${OUTDIR}/bin/gcc" ]; then
     echo "*** Building gcc (target)"
     rm -rf build/gcc-target
     mkdir -p build/gcc-target
     cd build/gcc-target
-    CFLAGS="--sysroot ${TOOLCHAIN}" CXXFLAGS="--sysroot ${TOOLCHAIN}" ../../userland/gcc-9.2.0/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --enable-languages='c,c++' --prefix=${OUTDIR}
+    CFLAGS="--sysroot ${SYSROOT}" CXXFLAGS="--sysroot ${SYSROOT}" ../../userland/gcc-9.2.0/configure --host=${TARGET} --target=${TARGET} --disable-nls --disable-werror --enable-languages='c,c++' --prefix=/usr
+    #--with-gxx-include-dir=${OUTDIR}/usr/include/c++/9.2.0
     # XXX for some reason, we need to explicitely configure/build the host
     # libcpp; otherwise it inherits our sysroot which messes things up...
     make ${MAKE_ARGS} configure-build-libcpp
     (cd build-x86_64-pc-linux-gnu/libcpp && make ${MAKE_ARGS})
     make ${MAKE_ARGS}
-    make ${MAKE_ARGS} install
+    make ${MAKE_ARGS} install DESTDIR=${OUTDIR}
     cd ../..
 fi
 
-if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+if [ ! -f "${TARGET}/lib/libc.a" ]; then
     echo "*** Building newlib (target)"
     rm -rf build/newlib-target
     mkdir build/newlib-target
     cd build/newlib-target
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${OUTDIR} ../../lib/newlib-3.1.0
     ninja install
-    mv ${OUTDIR}/usr/include ${OUTDIR}/${TARGET}/include
+    #mv ${OUTDIR}/usr/include ${OUTDIR}/${TARGET}/include
     cd ../..
 fi
 
-TOOLCHAIN_TARGET=1
-if [ "$TOOLCHAIN_TARGET" -ne 0 ]; then
+if [ ! -f "${TARGET}/include/dogfood/types.h" ]; then
     echo "*** Installing kernel headers (target)"
     rm -rf build/headers-target
     mkdir build/headers-target
     cd build/headers-target
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DCMAKE_INSTALL_PREFIX=${OUTDIR} ../../kernel-headers
     ninja install
-    mv ${OUTDIR}/usr/include/dogfood ${OUTDIR}/${TARGET}/include
-    rmdir ${OUTDIR}/usr/include
+    #mv ${OUTDIR}/usr/include/dogfood ${OUTDIR}/${TARGET}/include
     cd ../..
 fi
