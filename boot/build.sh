ac2d156f4b358d5f54c4210b584eb2b7c20833da
Rink Springer <rink@rink.nu>
2019-11-22 13:56:28 +0100

    Remove obsolete files

--- boot/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-# Note: assumes you did: mkdir build && cd build && cmake -GNinja ..
-(cd ../build && ninja clean && ninja kernel_mb)
-cp ../build/kernel/kernel.mb iso
-grub-mkrescue -o dogfood.iso iso
--- boot/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#qemu-system-x86_64 -serial stdio -hda /home/rink/github/dogfood/boot/ext2.img -cdrom /home/rink/github/dogfood/boot/dogfood.iso -boot d $@
-qemu-system-x86_64 -serial stdio -hda /tmp/ext2.img -cdrom /home/rink/github/dogfood/boot/dogfood.iso -boot d $@
