22d1c33bf12077717016c7bdae958ac9ecb286e0
Rink Springer <rink@rink.nu>
2019-11-28 19:08:57 +0100

    Add Dockerfile's for build images
    This also includes a convenient script to build them

---
+++ create-docker-images.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+docker 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Docker not found; please install and re-run"
+    exit 1
+fi
+
+set +e
+
+BUILD_DIR=`mktemp -d`
+trap "rm -rf ${BUILD_DIR}" EXIT
+echo "Preparing build directory; ${BUILD_DIR}"
+cp ci/toolchain/Dockerfile ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/work/userland
+cp -r "userland/binutils-2.32" ${BUILD_DIR}/work/userland
+cp -r "userland/gcc-9.2.0" ${BUILD_DIR}/work/userland
+cp -r settings.sh ${BUILD_DIR}/work
+cp -r build-toolchain.sh ${BUILD_DIR}/work
+
+docker build -t dogfood-toolchain:latest -f ${BUILD_DIR}/Dockerfile ${BUILD_DIR}
+docker build -t dogfood-buildimage:latest ci/build-image
